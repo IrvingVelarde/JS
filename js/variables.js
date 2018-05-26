699f1be8d306eff65502d02f788d17f4837debf4
Irvin Velarde <irvinvelarde@gmail.com>
2018-05-26 12:19:54 -0500

    Cambios en ...

--- js/variables.js
+++ js/variables.js
@@ -1,5 +1,5 @@
 // asignando una variable al nombre
-//var nombre = "Irvin Velarde";
+var nombre = "Irvin Velarde";
 var nombre;
 // Pedimos a travez de un prompt un valor que se a asignara a la variable que esta asignada
 nombre = prompt('Ingrese el nombre: ');
@@ -7,11 +7,17 @@ nombre = prompt('Ingrese el nombre: ');
 alert("Mi nombre es: " + nombre );
 var edad = 23;
 // Nos permite escribir algo por decirlo asi en el body del navegador
-document.write("Mi nombre es: " + nombre + "Y mi edad es: " + edad);
+document.write("Mi nombre es: " + nombre + " </br>Y mi edad es: " + edad + "</br>");
 // Nos permite ver en la consola del navegador
 console.log(nombre);
 
 var n = prompt("Ingrese el valor");
 var m = prompt("Ingrese el valor");
 var suma = parseInt(n) + parseInt(m)
-document.write("La suma es: " + suma); 
+document.write("La suma es: " + suma + "</br>"); 
+//  Mostramos un mensaje de manera directa
+document.write("Este es un mensaje </br>")
+// Mostramos un mensaje con comillas
+document.write('Esto es un mensaje de: "Irvin Velarde" </br>');
+document.write("Esto es un mensaje de: \"Irvin Velarde\" </br>");
+document.write("Esto es un mensaje de: \'Irvin Velarde\'");
