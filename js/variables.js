4c4708fd4a7713f8f5367e3f8390a2a13e73fa01
Irvin Velarde <irvinvelarde@gmail.com>
2018-05-26 11:49:37 -0500

    Iniciando JavaScript

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+// asignando una variable al nombre
+//var nombre = "Irvin Velarde";
+var nombre;
+// Pedimos a travez de un prompt un valor que se a asignara a la variable que esta asignada
+nombre = prompt('Ingrese el nombre: ');
+// Nos muestra una alerta
+alert("Mi nombre es: " + nombre );
+// Nos permite escribir algo por decirlo asi en el body del navegador
+document.write("Mi nombre es: " + nombre);
+// Nos permite ver en la consola del navegador
+console.log(nombre);
+
+var n = prompt("Ingrese el valor");
+var m = prompt("Ingrese el valor");
+var suma = parseInt(n) + parseInt(m)
+document.write("La suma es: " + suma); 
